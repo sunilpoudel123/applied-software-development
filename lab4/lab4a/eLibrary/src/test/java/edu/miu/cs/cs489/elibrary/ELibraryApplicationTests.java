bf48ce44b94c6d75aecd45b060ff51683efa8c77
Sunil Poudel <sunil.poudel@miu.edu>
2025-10-03 11:32:47 -0500

    solution for lab 4 part a is added

---
+++ lab4/lab4a/eLibrary/src/test/java/edu/miu/cs/cs489/elibrary/ELibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.cs489.elibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
